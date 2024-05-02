4dacd58e8c5fee3178c9537253af6858e479b357
Adityakaaltatva <researchphyics@gmail.com>
2024-05-02 23:00:39 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript,React and Solidity these days.
+   Anyway regardless of the language, I just like programming. 
+    Here is my <a href="https://github.com/adityaKaaltatva" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree Which is yet I am going to get in 2 years. It will be written as Aditya Pandey Full stack web3 AI cloud Developer`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also Love working with cloud and Blockchain. As I am The Great Adityakaaltatva
+  `,
+  tv: `That's my TV. I've been watching tech youtubers and Political channels a lot recently  :
+  I have interest in psychology and spirituality also`,
+   
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/adityaKaaltatva" target="_blank">Check it out?</a>
+  Contact me at realaaditya5@gmail.com.com if you have any interesting job opportunities!`,
+  projects: `I am Working on some API projects and terminal based portfolio project, But I have a good experience in working 
+  In Blockchain Industry and Now I am learning CLoud
+  `,
+  library: `There are a lot of programming books on my shelves. I like Stoic phylosphy also But my favorurite Book is BhagvatGeeta
+  Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Aditya Pandey portfolio, just close the tab.`,
+};
